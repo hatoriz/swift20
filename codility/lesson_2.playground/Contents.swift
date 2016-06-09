56215e2375ac4d91d02d3655a707578608c37462
hatoriz <ninja.hatoriz@gmail.com>
2016-06-10 00:06:56 +0700

    [codility] solved lesson 2 cyclic rotation

--- codility/lesson_2.playground/Contents.swift
+++ codility/lesson_2.playground/Contents.swift
@@ -1,5 +1,29 @@
-//: Playground - noun: a place where people can play
+import Foundation
 
-import UIKit
 
-var str = "Hello, playground"
+public func solution(inout A : [Int], _ K : Int) -> [Int] {
+
+    let length = A.count
+    
+    guard length > 1  else {
+     return A
+    }
+    
+    if K == 0 {
+        return A
+    }
+    
+    for index in 1...K {
+        A.insert(A[A.count-index], atIndex: A.startIndex)
+    }
+    
+    for index in 1...K {
+        A.removeAtIndex(length + K - index)
+    }
+    
+    return A
+}
+
+var testArr = [0,1,2,3,4,5,6,7,8,9,1,2,3,4,5]
+var outArr = solution(&testArr,12)
+

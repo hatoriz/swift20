b26a125e212472c26e52303d518ff37cc696a3e3
Hatoriz <Ninja.hatoriz@gmail.com>
2016-06-09 14:26:48 +0700

    [codility] lesson 2

--- codility/lesson_2.playground/Contents.swift
+++ codility/lesson_2.playground/Contents.swift
@@ -1,5 +1,18 @@
-//: Playground - noun: a place where people can play
+// Lesson 2-1 Cyclic Rotation
+import Foundation
 
-import UIKit
+public func solution(inout A: [Int], _K: Int) -> [Int]{
+    var newArray: [Int] = []
 
-var str = "Hello, playground"
+    //add "_K" element from A into newArray from backward
+    for index in 0..<_K {
+        print(index)
+        newArray[index] += A[A.count-index]
+    }
+    
+    //add the rest of A for N-K to newArray
+    return [1,2]
+}
+
+var A = [0,1,2,3,4,5,6,7]
+solution(&A, _K: 3)
